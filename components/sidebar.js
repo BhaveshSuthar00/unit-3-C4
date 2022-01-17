c73981eb484a734cc3c1337ebdd00517362c2a45
BhaveshSuthar00 <bhaveshs0770@gmail.com>
2022-01-17 17:33:09 +0530

    c

---
+++ components/sidebar.js
@@ -0,0 +1,14 @@
+function sidebar() {
+    return `
+    <a class="side_h3tag" href="./index.html"><h3>Techcrunch</h3></a>
+        <p class="side_ptag">login</p>
+        <input id="search" type="text" placeholder="Search">
+        <p class="side_ptag">startups</p>
+        <p class="side_ptag">Newsletters</p>
+        <p class="side_ptag">Audio</p>
+        <p class="side_ptag">Video</p>
+    `
+}
+// return your html component here
+//Make sure to give input search box id as ""
+    export default sidebar
